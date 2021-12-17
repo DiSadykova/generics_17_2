8f8ee8df0f5263e4607f965ab2b4207b0d8ddba6
Di.Sadykova <dilyara.mozhet>
2021-12-17 14:39:40 +0300

    подготовлено решение

--- generics_17_2/Program.cs
+++ generics_17_2/Program.cs
@@ -10,48 +10,85 @@ namespace generics_17_2
     {
         static void Main(string[] args)
         {
-            Account<int> account1 = new Account<int>(12, 125000, "Сергей") ;
-            Console.WriteLine(account1.AccOwner);
+            Console.Write("Введите номер счета: ");
+            int accNumInt=0;
+            try
+            {
+                accNumInt = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            { 
+            Console.Write("{0},Введите целочисленное значение: ", ex.Message);
+                accNumInt = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();        
+            }
+            catch (OverflowException ex)
+            {
+                Console.Write("{0},Введите номер счета: ", ex.Message);
+                accNumInt = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+            }
+            Console.Write("Введите баланс счета: ");
+            double accBalance=0;
+            try
+            {
+                accBalance = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.Write("{0} , введите баланс счета: ", ex.Message);
+                accBalance = Convert.ToDouble(Console.ReadLine());
 
+            }
+            Console.Write("Введите ФИО владельца счета: ");
+            string accOwner = Console.ReadLine();
+            Account<int> account1 = new Account<int>(accNumInt, accBalance, accOwner) ;
+
+
+            Console.Write("Введите номер счета: "); 
+            string accNumString = Console.ReadLine();
+            Console.Write("Введите баланс счета: ");
+            try
+            {
+                accBalance = Convert.ToDouble(Console.ReadLine()); 
+            }
+            catch (FormatException ex) 
+            {
+                Console.Write("{0} , введите баланс счета: ", ex.Message);
+                accBalance = Convert.ToDouble(Console.ReadLine());
+
+            }
+            Console.Write("Введите ФИО владельца счета: ");
+            accOwner = Console.ReadLine();
+            Account<string> account2 = new Account<string>(accNumString, accBalance, accOwner);
+
+            Account<int> account3 = new Account<int>(14334455, 244, "Иванов Иван Иванович");
+            Account<string> account4 = new Account<string>("4666665544457778883332аа", 345.5, "Сергеев Сергей Сергеевич");
+            account1.AccInfo();
+            account2.AccInfo();
+            account3.AccInfo();
+            account4.AccInfo();
             Console.ReadKey();
         }
     }
     class Account<T>
     {
-        private T accNumPole;
-        private double accBalancePole;
-        private string accOwnerPole;
+        T AccNum { get; set; }
+        double AccBalance { get; set; }
+        string AccOwner { get; set; }
 
-        public T AccNum
-        {
-            set
-            { accNumPole = value; }
-            get
-            { return accNumPole; }
-        }
-        public double AccBalance
-        {
-            set { accBalancePole = value; }
-            get { return accBalancePole; }  
-        }
-        public string AccOwner
-        {
-            set { accOwnerPole = value; }  
-            get { return accOwnerPole; }
-        }
+       
         public Account(T accNum, double accBalance, string accOwner)
         {
             AccNum = accNum;
             AccBalance = accBalance;
             AccOwner = accOwner;
-            AccNum = accNum;
-            AccBalance = accBalance;
-            AccOwner = accOwner;
         }
-        public void SetAccount()
+        public void AccInfo()
         {
-            Console.Write("Введите номер счета: ");
-            
+            Console.WriteLine(AccNum);
+            Console.WriteLine(AccBalance);
+            Console.WriteLine(AccOwner);
         }
     }
 }
